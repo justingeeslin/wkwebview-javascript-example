cc20b2bd4ff0aaf60244016fe4f29a04ccc738a0
Justin Geeslin <justin@geesl.in>
2018-05-10 11:17:36 -0500

    wkwebview-javascript-init

--- licon-health/ViewController.swift
+++ licon-health/ViewController.swift
@@ -7,25 +7,33 @@
 //  Inspired by https://www.hackingwithswift.com/read/32/3/how-to-use-sfsafariviewcontroller-to-browse-a-web-page
 
 import UIKit
-import SafariServices
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://healthcare.gov")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript("$(document.body).hide()", completionHandler: nil)
     }
 
+    override func loadView() {
+        webView = WKWebView()
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     @IBAction func openTouchDown(_ sender: Any) {
-        if let url = URL(string: "https://healthcare.gov") {
-            let config = SFSafariViewController.Configuration()
-            config.entersReaderIfAvailable = false
-            config.barCollapsingEnabled = true
-            
-            let svc = SFSafariViewController(url: url, configuration: config)
-            
-            present(svc, animated: true)
-        }
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
